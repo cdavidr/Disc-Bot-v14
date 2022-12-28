df414b8b3fc9c69c85c2e93bdfb150e0f9f6d5be
David Rubiano <david.rubiano@endava.com>
2022-12-28 12:00:51 -0500

    it7-pick

--- events/commands/pick.js
+++ events/commands/pick.js
@@ -37,61 +37,17 @@ const invoke = (interaction) => {
 	let pickAmount = interaction.options.get('amount');
 	let randomizedSpots = new Array(numOfMembers).fill().map((a, i) => a = i).sort(() => Math.random() - 0.5);
 	let winners = randomizedSpots.slice(0, pickAmount.value);
-	for (const idx of winners) {
-		console.log(rows[idx].Discord);
-		console.log(rows[idx]['Default Burner']);
+	let winnersText = "";
 
+	for (const idx of winners) {
+		winnersText += rows[idx].Discord + "," + rows[idx]['Default Burner'] + "\n\n";
 	}
 
 	// Create a MessageEmbed and add an inlined field for each property displayed in the reply message
 	const embed = new EmbedBuilder().setTitle(guild.name).addFields([
 		{
-			name: 'Members',
-			value: guild.memberCount.toString(),
-			inline: true,
-		},
-		{
-			name: 'Created At',
-			value: guild.createdAt.toLocaleDateString('de-DE', {
-				day: '2-digit',
-				month: '2-digit',
-				year: 'numeric',
-			}),
-			inline: true,
-		},
-		{
-			name: 'ID',
-			value: guild.id,
-			inline: true,
-		},
-		{
-			name: 'AFK channel',
-			value: guild.afkChannel?.name ?? 'None',
-			inline: true,
-		},
-		{
-			name: 'AFK timeout',
-			value: guild.afkTimeout.toString(),
-			inline: true,
-		},
-		{
-			name: 'Custom URL',
-			value: guild.vanityURLCode ?? 'None',
-			inline: true,
-		},
-		{
-			name: 'Boosts',
-			value: guild.premiumSubscriptionCount.toString(),
-			inline: true,
-		},
-		{
-			name: 'Discord Partner',
-			value: guild.partnered ? 'Yes' : 'No',
-			inline: true,
-		},
-		{
-			name: 'Verified',
-			value: guild.verified ? 'Yes' : 'No',
+			name: 'Winners',
+			value: winnersText,
 			inline: true,
 		},
 	]);
